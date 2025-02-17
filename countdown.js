569779f01e0e2bd49795701c2ef5d601dd5d2a9c
abbyqiu <45708602+abbyqiu@users.noreply.github.com>
2025-02-17 16:08:00 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,14 +1,19 @@
 function updateCountdownDisplay(endDate, displayElementId, type) {
     const countDownDate = new Date(endDate).getTime();
     const now = new Date().getTime();
+    const nowYear = new Date().getFullYear();
     const distance = countDownDate - now;
  
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
- 
-    document.getElementById(displayElementId).innerHTML = `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
+    if(type === 1){
+        document.getElementById(displayElementId).innerHTML = `距离下班还有：${hours}小时 ${minutes}分钟 ${seconds}秒`;
+    }else {
+        // document.getElementById(displayElementId).innerHTML = `今年还有：${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
+        document.getElementById(displayElementId).innerHTML = `${nowYear}年还剩下：${days}天`;
+    }
 }
  
 // 获取当前年份，用于元旦的日期计算
