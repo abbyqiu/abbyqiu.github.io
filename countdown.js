1fdff0d8eb567a948191be5e53f8dc821bb142eb
abbyqiu <45708602+abbyqiu@users.noreply.github.com>
2025-02-17 18:28:41 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -9,7 +9,8 @@ function updateCountdownDisplay(endDate, displayElementId, type) {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     if(type === 1){
-        document.getElementById(displayElementId).innerHTML = `距离下班还有：${hours}小时 ${minutes}分钟 ${seconds}秒`;
+        const timeStr = seconds < 0 ? `--小时 --分钟 --秒` : `${hours}小时 ${minutes}分钟 ${seconds}秒`
+        document.getElementById(displayElementId).innerHTML = `距离下班还有：${timeStr}`;
     }else {
         // document.getElementById(displayElementId).innerHTML = `今年还有：${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
         document.getElementById(displayElementId).innerHTML = `${nowYear}年还剩下：${days}天`;
@@ -22,8 +23,10 @@ const currentMonth = new Date().getMonth() + 1;
 const currentDay = new Date().getDate();
 const workEndDate = `${currentYear}-${currentMonth}-${currentDay} 17:30:00`;
 const newYearDate = `${currentYear}-12-31 17:30:00`;
- 
+const WeddingDate = '2025-1-05-11 00:00:00' 
 // 设置下班倒计时
 updateCountdownDisplay(workEndDate, 'workEndCountdown', 1);
 // 设置元旦倒计时
 updateCountdownDisplay(newYearDate, 'newYearCountdown', 2);
+// 设置刘小志婚礼倒计时
+updateCountdownDisplay(WeddingDate, 'weddingCountdown', 2);
